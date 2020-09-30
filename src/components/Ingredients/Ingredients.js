11170e3e1237c283b013ae3e9a2e3abfd4dd5491
AmirArreaza <ajaarreazav@gmail.com>
2020-09-30 14:47:00 +0100

    Data is now saved in Firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,10 +8,28 @@ const Ingredients = () => {
   const [userIngredients, setUserIngridients] = useState([]);
 
   const addIngridientHandler = (ingredient) => {
-    setUserIngridients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch("https://react-hooks-update-66a24.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify({ ingredient }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((body) => {
+        setUserIngridients((prevIngredients) => [
+          ...prevIngredients,
+          { id: body.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngridients((prevIngredients) =>
+      prevIngredients.filter((ing) => ing.id !== ingredientId)
+    );
   };
 
   return (
@@ -20,7 +38,10 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
